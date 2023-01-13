468b417a7ff539b72d5cc5acea7ead27b888cdf8
n.luparev <nikita.luparev@gmail.com>
2023-01-14 01:02:39 +0500

    add routes

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,17 @@
 import * as express from "express";
+import * as morgan from "morgan";
+
+import router from "./router";
 
 const app = express();
+
+app.use(morgan("dev"));
 
 app.get("/", (req, res) => {
   res.status(200);
   res.json({ msg: "hello" });
 });
+
+app.use("/api", router);
 
 export default app;
