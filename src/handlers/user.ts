9d283eed46c0d6d4a820c8f87c8122c90cc302c0
n.luparev <nikita.luparev@gmail.com>
2023-02-07 00:38:10 +0500

    helper for sing in process

--- src/handlers/user.ts
+++ src/handlers/user.ts
@@ -1,13 +1,8 @@
 import prisma from "../db";
-import { createJWT, hashPassword } from "../modules/auth";
+import { createJWT, hashPassword, comparePasswords } from "../modules/auth";
 
 export const createNewUser = async (req, res) => {
   const hash = await hashPassword(req.body.password);
-
-  if (!req.body) {
-    res.status(401);
-    res.send("Not sufficient data provided!");
-  }
 
   const { username } = req.body;
 
@@ -20,5 +15,22 @@ export const createNewUser = async (req, res) => {
 
   const token = createJWT(user);
 
+  res.json({ token });
+};
+
+export const signin = async (req, res) => {
+  const user = await prisma.user.findUnique({
+    where: { id: req.body.username },
+  });
+
+  const isValid = await comparePasswords(req.body.password, user.password);
+
+  if (!isValid) {
+    res.status(401);
+    res.send("Invalid username or password");
+    return;
+  }
+
+  const token = createJWT(user);
   res.json({ token });
 };
