ede8ded82e88d26e91e4e702020b366fe287b9a0
n.luparev <nikita.luparev@gmail.com>
2023-02-07 22:18:59 +0500

    add validation support

--- src/router.ts
+++ src/router.ts
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import { body, validationResult } from "express-validator";
 
 const router = Router();
 /**
@@ -12,7 +13,18 @@ router.get("/product/:id", (req, res) => {});
 
 router.post("/product", (req, res) => {});
 
-router.put("/product/:id", (req, res) => {});
+router.put("/product/:id", body("name").isLength({ min: 4 }), (req, res) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    res.status(400);
+    res.json({ errors: errors.array() });
+    return;
+  }
+
+  res.status(200);
+  res.json({ products: [] });
+});
 
 router.delete("/product/:id", (req, res) => {});
 
